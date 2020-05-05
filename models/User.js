31f133c98f6314a65411040bccc873af9062c944
Manuver Gujjar <Manvvergujjar@gmail.com>
2020-05-05 11:09:46 +0530

    updated schema

--- models/User.js
+++ models/User.js
@@ -1,83 +1,84 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-const crypto = require("crypto");
-var bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const crypto = require('crypto');
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      lowercase: true,
-      required: [true, "can't be blank"],
-      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
-      index: true,
-      unique: true,
+    {
+        username: {
+            type: String,
+            lowercase: true,
+            required: [true, "can't be blank"],
+            match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+            index: true,
+            unique: true,
+        },
+        password: String,
+        desc: String,
+        isSuperUser: {
+            type: Boolean,
+            default: false,
+        },
+        filedata: [
+            {
+                fieldname: String,
+                originalname: String,
+                encoding: String,
+                mimetype: String,
+                destination: String,
+                filename: String,
+                path: String,
+                size: Number,
+                className: String,
+            },
+        ],
     },
-    password: String,
-    desc: String,
-    isSuperUser: {
-      type: Boolean,
-      default: false,
-    },
-    filedata: [
-      {
-        fieldname: String,
-        originalname: String,
-        encoding: String,
-        mimetype: String,
-        destination: String,
-        filename: String,
-        path: String,
-        size: Number,
-      },
-    ],
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-UserSchema.plugin(uniqueValidator, { message: "This user is already taken" });
-UserSchema.methods.setPassword = function(password) {
-  this.salt = crypto.randomBytes(16).toString("hex");
-  this.hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
+UserSchema.plugin(uniqueValidator, { message: 'This user is already taken' });
+UserSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto
+        .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+        .toString('hex');
 };
 
 // Authenticate input on database
-UserSchema.statics.authenticate = function(username, password, callback) {
-  User.findOne({ username: username }).exec(function(err, user) {
-    if (err) {
-      return callback(err);
-    } else if (!user) {
-      var err = new Error("User not found.");
-      err.status = 401;
-      return callback(err);
-    }
-    bcrypt.compare(password, user.password, function(err, result) {
-      if (result === true) {
-        return callback(null, user);
-      } else {
-        return callback();
-      }
+UserSchema.statics.authenticate = function (username, password, callback) {
+    User.findOne({ username: username }).exec(function (err, user) {
+        if (err) {
+            return callback(err);
+        } else if (!user) {
+            var err = new Error('User not found.');
+            err.status = 401;
+            return callback(err);
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result === true) {
+                return callback(null, user);
+            } else {
+                return callback();
+            }
+        });
     });
-  });
 };
 
 // Hashing password before saving it to the database
-UserSchema.pre("save", function(next) {
-  var user = this;
-  if (!this.isModified("password")) {
-    return next();
-  }
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (!this.isModified('password')) {
+        return next();
     }
-    user.password = hash;
-    next();
-  });
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
 });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
